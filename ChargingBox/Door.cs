9399c96bbe6186fa29bd4e10823318e264b006ed
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-03-04 14:17:33 +0100

    Added IDoor and Door

---
+++ ChargingBox/Door.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargingBox
+{
+    public class Door : IDoor
+    {
+        void LockDoor()
+        {
+
+        }
+
+        void UnlockDoor()
+        {
+
+        }
+    }
+}
---
+++ ChargingBox/IDoor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargingBox
+{
+    interface IDoor
+    {
+        void LockDoor();
+        void UnlockDoor();
+    }
+}
