d7711517108c89981bb9e872f3d7e2049fefaedf
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-03-11 12:32:21 +0100

    updated ChargeControl

--- Charging.Test.NUnit/UnitTest1.cs
+++ Charging.Test.NUnit/UnitTest1.cs
@@ -4,9 +4,17 @@ namespace ChargingBox.Test.NUnit
 {
     public class Tests
     {
+        private StationControl uut;
+        private IChargeControl uutChargeControl;
+        private IDisplay uutDisplay;
+        private IDoor uutDoor;
+        private IRfidReader uutRfidReader;
+        private IUsbCharger uutUsbCharger;  
+
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
