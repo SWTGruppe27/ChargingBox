d64767c63898b050a0e79a45a6dd3ad440db9d48
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-03-17 10:00:09 +0100

    stationcontroltest updated

--- Charging.Test.NUnit/StationControlTest.cs
+++ Charging.Test.NUnit/StationControlTest.cs
@@ -11,7 +11,7 @@ namespace ChargingBox.Test.NUnit
         private IDisplay uutDisplay;
         private IDoor uutDoor;
         private IRfidReader uutRfidReader;
-        private IUsbCharger uutUsbCharger;  
+        private IUsbCharger uutUsbCharger;
 
         [SetUp]
         public void Setup()
@@ -26,9 +26,102 @@ namespace ChargingBox.Test.NUnit
         }
 
         [Test]
-        public void StationControl()
+        public void StationControl_DoorOpen_DisplayConnectPhone()
         {
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() {_doorOpen = true});
+            uutDisplay.Received(1).ConnectPhone();
+        }
 
+        [Test]
+        public void StationControl_DoorClosed_DisplayScanRfid()
+        {
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutDisplay.Received(1).ScanRfid();
+        }
+
+        [Test]
+        public void StationControl_RfidDetectedBoxAvailable_DisplayConnectionToChargerFailed()
+        {
+            uutChargeControl.IsConnected().Returns(false);
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() {Id = 12});
+
+            uutDisplay.Received(1).ConnectionToChargerFailed();
+        }
+
+        [Test]
+        public void StationControl_RfidDetectedBoxAvailable_DisplayChargingBoxLocked()
+        {
+            uutChargeControl.IsConnected().Returns(true); 
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+
+            uutDisplay.Received(1).ChargingBoxLocked();
+        }
+
+        [Test]
+        public void StationControl_RfidDetectedBoxAvailable_DoorLockedCorrect()
+        {
+            uutChargeControl.IsConnected().Returns(true);
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+
+            uutDoor.Received(1).LockDoor();
+        }
+
+        [Test]
+        public void StationControl_RfidDetectedBoxAvailable_StartChargeCorrect()
+        {
+            uutChargeControl.IsConnected().Returns(true);
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+
+            uutChargeControl.Received(1).StartCharge();
+        }
+
+        [Test]
+        public void StationControl_RfidDetectedBoxLocked_StopChargeCorrect()
+        {
+            uutChargeControl.IsConnected().Returns(true);
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() {Id = 12});
+
+            uutChargeControl.Received(1).StopCharge();
+        }
+
+        [Test]
+        public void StationControl_RfidDetectedBoxLocked_DoorUnlockdCorrect()
+        {
+            uutChargeControl.IsConnected().Returns(true);
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+
+            uutDoor.Received(1).UnlockDoor();
+        }
+
+        [Test]
+        public void StationControl_RfidDetectedBoxLocked_DisplayRemovePhone()
+        {
+            uutChargeControl.IsConnected().Returns(true);
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+
+            uutDisplay.Received(1).RemovePhone();
+        }
+
+        [TestCase(11)]
+        [TestCase(13)]
+        public void StationControl_RfidDetectedBoxLocked_DisplayRfidWrong(int rfid)
+        {
+            uutChargeControl.IsConnected().Returns(true);
+            uutDoor.DoorChangedStateEvent += Raise.EventWith(new DoorChangedStateEventArgs() { _doorOpen = false });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = 12 });
+            uutRfidReader.ReadIdEvent += Raise.EventWith(new ReadIdEventArgs() { Id = rfid });
+
+            uutDisplay.Received(1).RfidError();
         }
     }
 }
