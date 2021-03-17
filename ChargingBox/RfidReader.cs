69b700e73f167787034fefbf7365b98e358ab3d6
Nugget98 <61585323+Nugget98@users.noreply.github.com>
2021-03-17 08:59:25 +0100

    Added Exception to FridReader

--- ChargingBox/RfidReader.cs
+++ ChargingBox/RfidReader.cs
@@ -6,13 +6,28 @@ namespace ChargingBox
 {
     public class RfidReader : IRfidReader
     {
+        public class NegativeIdException : Exception
+        {
+            public NegativeIdException()
+            {
+
+            }
+        }
+
         private int _id;
         public event EventHandler<ReadIdEventArgs> ReadIdEvent;
 
         public void SetId(int id)
         {
-            _id = id;
-            ReadId(new ReadIdEventArgs {Id = _id});
+            if (id < 0)
+            {
+                throw new NegativeIdException();
+            }
+            else
+            {
+                _id = id;
+                ReadId(new ReadIdEventArgs { Id = _id });
+            }
         }
         protected virtual void ReadId(ReadIdEventArgs e)
         {
